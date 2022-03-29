5e3f031d9218d65edf6cae43b2392002c61a23fe
Angel Santana <angel.d.santana00@gmail.com>
2022-03-29 11:53:28 -0500

    TESTING

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,7 +20,36 @@ public class HelloWorldServlet extends HttpServlet {
     }
 }
 
+class PageHitCounter extends HttpServlet {
 
-//testing testing
-//TESTING TESTING
-/// why no upload
+    private int hitCount;
+
+    public void init() {
+        // Reset hit counter.
+        hitCount = 0;
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // This method executes whenever the servlet is hit
+        // increment hitCount
+        hitCount++;
+        PrintWriter out = response.getWriter();
+        String title = "Total Number of Hits";
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<h2 align = \"center\">" + hitCount + "</h2>\n" +
+                "</body> </html>");
+    }
+}
+
+
